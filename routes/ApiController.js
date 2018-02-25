1918bdc7cf109dd5ac183df550c4685db878ed63
aman <amannanrhe@gmail.com>
2018-02-25 10:24:23 -0700

    Add functionality to search
    Send request with query and page parameters

--- routes/ApiController.js
+++ routes/ApiController.js
@@ -1,7 +1,29 @@
 var express = require('express');
 var router = express.Router();
 var path = require('path');
+var https = require("https");
 
 router.get('/', function(req, res, next){
     res.sendFile(path.resolve(__dirname, '../views/index.html'));
 });
+
+router.get('/search', function(req, res, next){
+    var query = req.query.q;
+    var page = 1;
+    if(req.query.page){
+        page = req.query.page;
+    } 
+    https.get('https://www.googleapis.com/customsearch/v1?key=AIzaSyDFE3Wb8AgaBNV4iDxYpAhFkh3l_L7FYZw&cx=017841468666952230708:n0hlq663r-u&q='+query+'&searchType=image&start='+page,response=>{
+        response.setEncoding("utf8");
+        var resBody = "";
+        response.on("data", data => {
+            resBody += data;
+        });
+        response.on("end", () => {
+            resBody = JSON.parse(resBody);
+            res.send(resBody);
+        });
+    });
+});
+
+module.exports = router;
